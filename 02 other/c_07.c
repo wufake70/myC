d7270d0d726064e688bcdd2c77f01f40dfe4469c
wufake <wufake70@gmail.com>
2023-09-05 16:31:28 +0800

    ....

--- 02 other/c_07.c
+++ 02 other/c_07.c
@@ -14,26 +14,27 @@
 */
 
 threadpool* pool=NULL;
-char* path_arr[10000000];
 
-atomic_int path_arr_index=0,
-    txt_counts=0,
-    pdf_counts=0,
-    docx_counts=0,
-    pptx_counts=0,
-    csv_counts=0,
-    ppt_counts=0,
-    jpg_counts=0,
-    jpeg_counts=0,
-    png_counts=0,
-    gif_counts=0;
+atomic_int dir_counts= ATOMIC_VAR_INIT(0),
+    file_counts= ATOMIC_VAR_INIT(0),
+    txt_counts= ATOMIC_VAR_INIT(0),
+    pdf_counts= ATOMIC_VAR_INIT(0),
+    docx_counts= ATOMIC_VAR_INIT(0),
+    pptx_counts= ATOMIC_VAR_INIT(0),
+    csv_counts= ATOMIC_VAR_INIT(0),
+    ppt_counts= ATOMIC_VAR_INIT(0),
+    jpg_counts= ATOMIC_VAR_INIT(0),
+    jpeg_counts= ATOMIC_VAR_INIT(0),
+    png_counts= ATOMIC_VAR_INIT(0),
+    gif_counts= ATOMIC_VAR_INIT(0);
+
 
 void scan(void*arg)
 {
     char* path=(char*)arg;
     DIR *dir=opendir(path);
     if(dir==NULL){
-        printf("%s\t文件夹打开失败T_T\n",path);
+        printf("%s\tFail open DIR T_T\n",path);
         system("pause");
         exit(1);
     }
@@ -50,164 +51,111 @@ void scan(void*arg)
         strcpy(path_1,path);
         strcat(path_1,entry->d_name);
 
-        // path_2作为 threadpoolAdd(pool,scan,path_2)的参数需要存放在堆上，确保其不被修改
-        char* path_2=malloc(sizeof(char)*(strlen(path_1)+2));
-        strcpy(path_2,path_1);
-        // 获取文件(夹)路径信息
-        if(stat(path_2,&file_stat)!=0){
-            // printf("路径信息错误或没有权限T_T\n");
+        if(stat(path_1,&file_stat)!=0){
+            // printf("The path information wrong OR No permission T_T\n");
             continue;
         }
         
         // 判断文件(夹)打印名字
         if(S_ISDIR(file_stat.st_mode)){
+            // path_2作为 threadpoolAdd(pool,scan,path_2)的参数需要存放在堆上，确保其不被修改
+            char* path_2=malloc(sizeof(char)*(strlen(path_1)+10));
+            strcpy(path_2,path_1);
+            // 获取文件(夹)路径信息
             
             strcat(path_2,"/");
             threadpoolAdd(pool,scan,path_2);
-            // atomic_fetch_add(&dir_counts,1);
+            atomic_fetch_add(&dir_counts,1);
 
         }else{
             // atomic_fetch_add(&file_counts,1);
             // pdf
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".pdf")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&pdf_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
             // txt
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".txt")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&txt_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
             // docx
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".docx")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&docx_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
             // pptx 
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".pptx")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
+                atomic_fetch_add(&file_counts,1);
+                atomic_fetch_add(&pdf_counts,1);
                 pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
-                atomic_fetch_add(&pptx_counts,1);
                 continue;
 
             }
             // csv 
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".csv")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&csv_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
             // ppt 
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".ppt")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&ppt_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
             // jpg  
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".jpg")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&jpg_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
             // png  
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".png")==0){
-                // path_arr 不能存储太多数据，这里png 高达12万，存不下会闪退
-                if(atomic_load(&png_counts)>2000) continue;
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&png_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
             // gif 
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".gif")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&gif_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
             // jpeg 
             if(strrchr(entry->d_name,'.')&&strcmp(strrchr(entry->d_name,'.'),".jpeg")==0){
                 pthread_mutex_lock(&pool->mutexpool);
-                path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                while(path_arr[path_arr_index] == NULL){
-                    path_arr[path_arr_index] = malloc(strlen(path_1) + 1);
-                }                
-                strcpy(path_arr[path_arr_index], path_1);
-                pthread_mutex_unlock(&pool->mutexpool);
-                atomic_fetch_add(&path_arr_index,1);
+                atomic_fetch_add(&file_counts,1);
                 atomic_fetch_add(&jpeg_counts,1);
+                pthread_mutex_unlock(&pool->mutexpool);
                 continue;
 
             }
@@ -229,55 +177,43 @@ int main()
 
     pool=threadpoolinit(50);
     if(pool==NULL){
-        printf("无法创建线程池T_T!");
+        printf("Unable make threadpool T_T!");
         exit(1);
     }
 
     char* path=malloc(sizeof(char)*1024);
-    printf("请输入路径: ");
+    printf("Input path: ");
     scanf("%[^\n]",path);
+    path[1023]='\0';
     if(path[strlen(path)-1]!='/'){
         sprintf(path,"%s/",path);
     }
 
     time_t t1=time(NULL),t2;
-    printf("请稍等...\n");
-    scan(path);
+    printf("Wait...\n");
+    threadpoolAdd(pool,scan,path);
     waitThreadsEnd(pool);
     t2=time(NULL);
 
     sleep(2);
 
     threadpooldestroy(pool);
-    printf("该路径下共扫描到\n\t\t %d个pdf,\t%d个txt\
-                           \n\t\t %d个docx，\t%d个pptx\
-                           \n\t\t %d个csv，\t%d个ppt\
-                           \n\t\t %d个jpg，\t%d个jpeg\
-                           \n\t\t %d个png，\t%d个gif\
-                           。\n共耗时 %d秒。\n",\
+    printf("The path scan \n\t\t %d pdf,\t%d txt\
+                           \n\t\t %d docx,\t%d pptx\
+                           \n\t\t %d csv,\t%d ppt\
+                           \n\t\t %d jpg,\t%d jpeg\
+                           \n\t\t %d png,\t%d gif\
+                           \n\t\t %d dirs,\t%d Files\
+                           \nConsumed time %d sec.\n",\
                            pdf_counts,txt_counts,\
                            docx_counts,pptx_counts,\
                            csv_counts,ppt_counts,\
                            jpg_counts,jpeg_counts,\
                            png_counts,gif_counts,\
+                           dir_counts,file_counts,\
                            (int)(t2-t1));
 
-    // 写入操作
-    FILE* fp=NULL;
-    if((fp=fopen("path.txt","a+"))==NULL){
-        printf("打开失败!!\n");
-        exit(EXIT_FAILURE);
-    }
-    for(int i=0;i<path_arr_index;i++){
-        fputs(path_arr[i],fp);
-        fputs("\n",fp);
-    }
-    fclose(fp);
-
-    // 释放动态内存
-    for(int i=0;i<path_arr_index;i++){
-        free(path_arr[i]);
-    }
+    
 
     system("pause");
 
